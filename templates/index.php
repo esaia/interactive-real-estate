3b1fc06cd69dc083a113254fe101e6967cd80105
esaia <esaiagafrindashvili@gmail.com>
2024-09-17 21:11:20 +0400

    refactor: remove unnecessary jquery code

--- templates/index.php
+++ templates/index.php
@@ -1,20 +1,2 @@
-<?php
-
-
-
-
-
-// include_once plugin_dir_path(IRE_PLUGIN_FILE) . './templates/components/toast.php';
-
-
-
-// if (isset($_GET['project'])) {
-//     include_once plugin_dir_path(IRE_PLUGIN_FILE) . './templates/pages/single-project.php';
-// } else {
-//     include_once plugin_dir_path(IRE_PLUGIN_FILE) . './templates/pages/home.php';
-// }
-
-?>
-
 <div id="my-vue-app">
 </div>
--- templates/projects/getProjects.php
+++
@@ -1,5 +0,0 @@
-<div id="projectsContainer" class="grid grid-cols-3 xl:grid-cols-4 gap-4 mt-3">
-
-
-
-</div>
