1c6f744fae05feb14e5b1941fd8f1b62abd40733
esaia <esaiagafrindashvili@gmail.com>
2024-11-07 19:16:03 +0400

    refactor: svgcanvas select paths with left click

--- vue/src/components/SvgCanvas.vue
+++ vue/src/components/SvgCanvas.vue
@@ -66,6 +66,11 @@ const getSVGCoordinates = (event, svgElement) => {
 
 // Event handlers
 const onCanvasClick = (event) => {
+  if (event.target.nodeName === "path" && points.value?.length === 0) {
+    onPathContextMenu(event);
+    return;
+  }
+
   if (shapeClosed.value || updateMode.value) return;
 
   const svg = svgCanvas.value.querySelector("svg");
@@ -131,6 +136,12 @@ const circleMouseMove = (event) => {
 };
 
 const onCanvasMouseMove = throttle((event) => {
+  if (event.target.nodeName === "path" && !points.value?.length) {
+    svgCanvas.value.style.cursor = "pointer";
+  } else {
+    svgCanvas.value.style.cursor = "crosshair";
+  }
+
   setCursorValues(event);
 
   if (pan.value) {
